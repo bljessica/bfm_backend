0fb23eb7b11f8107907def04ca52ed1fad3badfd
bljessica <1194620082@qq.com>
2021-03-11 20:05:31 +0800

    feat: save data to db

--- db/connect.js
+++ db/connect.js
@@ -1,24 +1,104 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 // 使用原生promise，mongoose自带promise不再支持
 // mongoose.Promise = global.Promise
 
 const url = 'mongodb://127.0.0.1/bfm'
 
-mongoose.set('useCreateIndex', true);
+mongoose.set('useCreateIndex', true)
 mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log('数据库连接成功'))
   .catch((err) => console.log('数据库连接失败' + err))
 
-const db = mongoose.connection;
+const db = mongoose.connection
 
 //集合规则
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   openId: {
     type: Number,
     required: true,
     unique: true
   }
 })
+const bookSchema = new Schema({
+  bookName: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String
+  },
+  publisher: {
+    type: String
+  },
+  publishTime: {
+    type: String
+  },
+  coverSrc: {
+    type: String
+  },
+  bookIntroduction: {
+    type: String
+  },
+  authorIntroduction: {
+    type: String
+  },
+  score: {
+    type: String
+  },
+  bookType: {
+    type: String,
+    enum: ['计算机图书', '小说', '名著']
+  }
+})
+bookSchema.index({bookName: 1, bookType: 1}, {unique: true})
+const filmSchema = new Schema({
+  chineseName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  foreignName: {
+    type: String
+  },
+  coverSrc: {
+    type: String
+  },
+  introduction: {
+    type: String
+  },
+  relatedInfo: {
+    type: String
+  },
+  score: {
+    type: String
+  }
+})
+const musicSchema = new Schema({
+  musicName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  singer: {
+    type: String
+  },
+  publishTime: {
+    type: String
+  },
+  musicType: {
+    type: String
+  },
+  coverSrc: {
+    type: String
+  },
+  score: {
+    type: String
+  }
+})
 
 exports.User = mongoose.model('User', userSchema)
+exports.Book = mongoose.model('Book', bookSchema)
+exports.Film = mongoose.model('Film', filmSchema)
+exports.Music = mongoose.model('Music', musicSchema)
---
+++ utils/saveDataToDB.js
@@ -0,0 +1,65 @@
+const { Book, Film, Music } = require('../db/connect')
+const fs = require('fs')
+
+function saveFilmsToDB () {
+  const data = fs.readFileSync('data/films_Top250.txt', 'utf8')
+  console.log('films_Top250.txt文件读取完成')
+  data.split('\n').forEach(async (line, idx) => {
+    const arr = line.split('\t')
+    await Film.create({
+      chineseName: arr[2],
+      foreignName: arr[3],
+      coverSrc: arr[1],
+      introduction: arr[6],
+      relatedInfo: arr[7],
+      score: arr[4]
+    }).catch(err => {
+      console.log(err)
+    })
+  })
+}
+
+function saveMusicsToDB () {
+  const data = fs.readFileSync('data/musics_Top250.txt', 'utf8')
+  console.log('musics_Top250.txt文件读取完成')
+  data.split('\n').forEach(async (line, idx) => {
+    const arr = line.split('\t')
+    await Music.create({
+      musicName: arr[1],
+      singer: arr[2],
+      publishTime: arr[3],
+      musicType: arr[4],
+      coverSrc: arr[7],
+      score: arr[5]
+    }).catch(err => {
+      console.log(err)
+    })
+  })
+}
+
+function saveBooksToDB (bookType, filePath) {
+  const data = fs.readFileSync(filePath, 'utf8')
+  console.log(filePath + '文件读取完成')
+  data.split('\n').forEach(async (line, idx) => {
+    const arr = line.split('\t')
+    await Book.create({
+      bookName: arr[0],
+      author: arr[1],
+      publisher: arr[2],
+      publishTime: arr[3],
+      coverSrc: arr[5],
+      bookIntroduction: arr[6],
+      authorIntroduction: arr[7],
+      score: arr[8],
+      bookType
+    }).catch(err => {
+      console.log(err)
+    })
+  })
+}
+
+saveFilmsToDB()
+saveMusicsToDB()
+saveBooksToDB('计算机图书', 'data/computer_books_info.txt')
+saveBooksToDB('名著', 'data/masterpiece_books_info.txt')
+saveBooksToDB('小说', 'data/novel_books_info.txt')
