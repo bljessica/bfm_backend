5223474cfbefc3c8e4b0953d1bec9c86b751a4f6
bljessica <1194620082@qq.com>
2021-04-26 00:48:03 +0800

    chore: add description

--- utils/saveDataToDB.js
+++ utils/saveDataToDB.js
@@ -3,56 +3,59 @@ const fs = require('fs')
 
 async function saveFilmsToDB () {
   const data = fs.readFileSync('data/films_Top250.txt', 'utf8')
-  console.log('films_Top250.txt文件读取完成')
   const dataArr = data.split('\n')
   for(let line of dataArr) {
     const arr = line.split('\t')
     const intro = arr[7]
     const intros = intro?.split('@')
-    const filmMakers = intros[0]?.split('/')[0]?.split('???')
+    const filmMakers = intros && intros[0]?.split('/')[0]?.split('???')
     if (/.*http.*/.test(arr[1])) {
-      await Film.create({
-        name: arr[2],
-        foreignName: arr[3],
-        coverSrc: arr[1],
-        introduction: arr[6],
-        director: filmMakers[0],
-        actors: filmMakers[1],
-        publishTime: intros[1],
-        country: intros[2],
-        type: intros[3],
-        score: arr[4]
-      }).catch(err => {
-        console.log(err)
-      })
+      const film = await Film.findOne({name: arr[2]})
+      if (!film) {
+        await Film.create({
+          name: arr[2],
+          foreignName: arr[3],
+          coverSrc: arr[1],
+          introduction: arr[6],
+          director: filmMakers[0],
+          actors: filmMakers[1],
+          publishTime: intros[1],
+          country: intros[2],
+          type: intros[3],
+          score: arr[4]
+        }).catch(err => {
+          console.log(err)
+        })
+      }
     }
   }
 }
 
 async function saveMusicsToDB () {
   const data = fs.readFileSync('data/musics_Top250.txt', 'utf8')
-  console.log('musics_Top250.txt文件读取完成')
   const dataArr = data.split('\n')
   for(let line of dataArr) {
     const arr = line.split('\t')
     if (/.*http.*/.test(arr[7])) {
-      await Music.create({
-        name: arr[1],
-        singer: arr[2],
-        publishTime: arr[3],
-        type: arr[4],
-        coverSrc: arr[7],
-        score: arr[5]
-      }).catch(err => {
-        console.log(err)
-      })
+      const music = await Music.findOne({name: arr[1]})
+      if (!music) {
+        await Music.create({
+          name: arr[1],
+          singer: arr[2],
+          publishTime: arr[3],
+          type: arr[4],
+          coverSrc: arr[7],
+          score: arr[5]
+        }).catch(err => {
+          console.log(err)
+        })
+      }
     }
   }
 }
 
 async function saveBooksToDB (type, filePath) {
   const data = fs.readFileSync(filePath, 'utf8')
-  console.log(filePath + '文件读取完成')
   const dataArr = data.split('\n')
   for(let line of dataArr) {
     const arr = line.split('\t')
@@ -84,8 +87,12 @@ async function saveBooksToDB (type, filePath) {
 
 (async function () {
   await saveMusicsToDB()
+  console.log('音乐数据存储完毕，开始存储电影数据...')
+  await saveFilmsToDB()
+  console.log('电影数据存储完毕，开始存储书籍数据...')
   await saveBooksToDB('计算机图书', 'data/computer_books_info.txt')
   await saveBooksToDB('名著', 'data/masterpiece_books_info.txt')
   await saveBooksToDB('小说', 'data/novel_books_info.txt')
-  await saveFilmsToDB()
+  console.log('书籍数据存储完毕')
+  process.exit()
 })()
